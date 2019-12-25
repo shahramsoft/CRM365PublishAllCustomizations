505dcac07352ab34bc172140c342823849918196
Shahram Foroozan <shahram.soft@gmail.com>
2019-12-25 06:29:54 -0800

    First Commit Of Project

---
+++ CRM365PublishCustomizations/CRM365PublishCustomizations/FrmPublish.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CRM365PublishCustomizations
+{
+    partial class FrmPublish
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnPublish = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnPublish
+            // 
+            this.BtnPublish.Location = new System.Drawing.Point(12, 12);
+            this.BtnPublish.Name = "BtnPublish";
+            this.BtnPublish.Size = new System.Drawing.Size(262, 23);
+            this.BtnPublish.TabIndex = 0;
+            this.BtnPublish.Text = "Publish";
+            this.BtnPublish.UseVisualStyleBackColor = true;
+            this.BtnPublish.Click += new System.EventHandler(this.BtnPublish_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 41);
+            this.Controls.Add(this.BtnPublish);
+            this.Name = "Form1";
+            this.Text = "Publish CRM 365 Sample";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnPublish;
+    }
+}
+
---
+++ CRM365PublishCustomizations/CRM365PublishCustomizations/FrmPublish.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Crm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Client;
+using System;
+using System.Windows.Forms;
+
+namespace CRM365PublishCustomizations
+{
+    public partial class FrmPublish : Form
+    {
+        public FrmPublish()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Get Organization Connection Data
+        /// </summary>
+        /// <returns>Organization to Use for Later Publish Action</returns>
+        public OrganizationServiceProxy GetOrgService()
+        {
+            try
+            {
+                Uri url = new Uri("http://IPAddress/OrganizationName/XRMServices/2011/Organization.svc");
+                System.ServiceModel.Description.ClientCredentials clientCredentials = new System.ServiceModel.Description.ClientCredentials();
+                clientCredentials.Windows.ClientCredential = new System.Net.NetworkCredential("UserName", "Password", "DomainName");
+
+                OrganizationServiceProxy orgService = new OrganizationServiceProxy(url, null, clientCredentials, null);
+                return orgService;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        /// <summary>
+        /// Btn Publish Click Action
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnPublish_Click(object sender, EventArgs e)
+        {
+          var organizationObj=  GetOrgService();
+            PublishEntity(organizationObj);
+        }
+        /// <summary>
+        /// Publish An Entity
+        /// </summary>
+        /// <param name="orgProxy">Connection Information</param>
+        /// <returns>Success Of Operation</returns>
+        public bool PublishEntity(OrganizationServiceProxy orgProxy)
+        {
+        try 
+	        {
+                 //===========================Begin Publish All=============================
+                PublishAllXmlRequest publishallxmlrequest = new PublishAllXmlRequest();
+                orgProxy.Execute(publishallxmlrequest);
+                //===========================End Publish All=============================
+
+
+                return true;
+            }
+            catch (Exception ex)
+        	{
+                MessageBox.Show(ex.Message);
+                return false;
+		
+	        }
+
+        }
+    }
+}
